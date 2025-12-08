f489ced72a72ee6a28145ec3e1f62ae9ad809c68
behnam-deriv <133759298+behnam-deriv@users.noreply.github.com>
2025-12-08 10:33:56 +0800

    fix: update contract marker left padding (#473)

--- packages/core/src/Stores/Helpers/chart-markers.js
+++ packages/core/src/Stores/Helpers/chart-markers.js
@@ -562,10 +562,10 @@ export function calculateMarker(
         }
     }
 
-    let contractMarkerLeftPadding = is_mobile ? 10 : 80;
+    let contractMarkerLeftPadding = is_mobile ? 10 : 100;
 
     if (is_resize_desktop && !is_mobile) {
-        contractMarkerLeftPadding = 330;
+        contractMarkerLeftPadding = 404;
     }
 
     // Helper to normalize profit value to a number or null
