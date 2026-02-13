778e7fc220d46dcb7992d43677c1c2fbe0a17f14
Akmal Djumakhodjaev <akmal@binary.com>
2026-02-13 17:50:24 +0800

    fix: add AR to the list of unsupported languages in default-options.ts

--- packages/shared/src/utils/constants/default-options.ts
+++ packages/shared/src/utils/constants/default-options.ts
@@ -4,4 +4,4 @@ import { isProduction } from '../config/config';
  * Returns List of unsupported languages based on the environment.
  * Supported languages: EN, ES, FR, PT, AR, IT, RU, BN, DE, KO, SW, VI, ZH_CN, ZH_TW, PL, SI, TA, MN
  */
-export const UNSUPPORTED_LANGUAGES = ['ID', 'TH', 'KM', 'UZ', 'TR'];
+export const UNSUPPORTED_LANGUAGES = ['ID', 'TH', 'KM', 'UZ', 'TR', 'AR'];
